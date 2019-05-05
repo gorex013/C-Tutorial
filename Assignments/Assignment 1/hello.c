f64266606f326777eaa6a2c82907b0e28729869e
Vasile Rotaru <vasilerotaru38@gmail.com>
2019-05-05 13:47:35 +0300

    Started tutorial

--- Assignments/Assignment 1/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* The main() function */
-int main(void) /*entry point*/
-{
-    /* write message to console */
-    puts("hello, 6.087 students");
-    return 0; /* exit(0 =>success)*/
-}
